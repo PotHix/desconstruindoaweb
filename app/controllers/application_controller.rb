17646368095d2e83570eeb43f9e9c75795e96740
PotHix <pothix@pothix.com>
2016-05-01 21:06:44 -0300

    It now uses HTTS... For sure.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_filter :strict_transport_security
+  def strict_transport_security
+    if request.ssl?
+      response.headers['Strict-Transport-Security'] = "max-age=31536000; includeSubDomains"
+    end
+  end
 end
